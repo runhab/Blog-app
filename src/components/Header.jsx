40b996af4240fd10a0ee76d9601795f1a76d3fc2
runhab <habfenethio@gmail.com>
2024-03-11 23:32:01 +0300

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../images/logo.png'
+// import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div>
-      Header
+      <nav>
+        <div className="container nav_container">
+          <Link to="/" className='nav_logo'>
+              <img src={Logo} alt="navbar Logo" />
+          </Link>
+          <ul className='nav_menu'>
+              <li><Link to="/profile">Hab Fen</Link></li>
+              <li><Link to="/create">Create Post</Link></li>
+              <li><Link to="/authors">Auhtors</Link></li>
+              <li><Link to="/logout">Logout</Link></li>
+          </ul>
+          <button className="nav_toggle-btn">
+              <AiOutlineClose/>
+          </button>
     </div>
+      </nav>
   )
 }
 
