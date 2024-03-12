42b56d200c714d75b726a7386f468ace22421fb1
runhab <habfenethio@gmail.com>
2024-03-12 22:07:04 +0300

    sign up added

--- src/pages/AuthorPosts.jsx
+++ src/pages/AuthorPosts.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
+import PostItem from '../components/PostItem'  
+
+
+import {DUMMY_POSTS} from '../data'
+
 
 const AuthorPosts = () => {
+
+  const [posts, setPosts] = useState(DUMMY_POSTS)
+  
   return (
-    <div>
-      AuthorPosts
-    </div>
+    <section className='author-posts'>
+          {posts.length>0 ? <div className='container posts_container'>
+              {
+              posts.map(({ id, thumbnail, category, title, desc, authorID }) =>
+                  <PostItem Key={id} postID={id} thumbnail={thumbnail} category={category} title={title}
+                      description={desc} authorID={authorID} />
+              )
+          }
+              
+          </div>:<h2 className='center'>No Posts Found</h2>}
+          
+    </section>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
 const Register = () => {
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2:'',
+
+  })
+
+
+  const changeInputHandler = (e) => {
+    setUserData(prevState => {
+      return {...prevState,[e.target.name] :e.target.value}
+    })
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <section className='register'>
+      <div className='content container'>
+        <h2>Sign Up</h2>
+        <form className='form register_form'>
+          <p className='form_error-message'>This is an error message</p>
+          <input type='text' placeholder='Full Name' name='name' value={userData.name} onChange={changeInputHandler} autoFocus />
+          <input type='text' placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler} autoFocus/>
+          <input type='password' placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler} autoFocus />
+          <input type='password' placeholder='Confirm Password' name='password2' value={userData.password2dhn} onChange={changeInputHandler} autoFocus />
+          <button type='submit' className='btn primary' >Register</button>
+          <small>Already have an account ? <Link to='/login'>sign in</Link></small>
+        </form>
+      </div>
+     
+    </section>
   )
 }
 
