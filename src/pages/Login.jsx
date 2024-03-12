b16d46afbde98d5cf5fa3576f9946ad90c9e53ce
runhab <habfenethio@gmail.com>
2024-03-12 22:15:52 +0300

    sign in added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
 const Login = () => {
+  const [userData, setUserData] = useState({
+   
+    email: '',
+    password: '',
+
+  })
+
+
+  const changeInputHandler = (e) => {
+    setUserData(prevState => {
+      return {...prevState,[e.target.name] :e.target.value}
+    })
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <section className='login'>
+      <div className='content container'>
+        <h2>Sign In</h2>
+        <form className='form login_form'>
+          <p className='form_error-message'>This is an error message</p>
+          <input type='text' placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler} autoFocus/>
+          <input type='password' placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler} autoFocus />
+          <button type='submit' className='btn primary' >Login</button>
+          <small>Dont have an account ? <Link to='/register'>sign up</Link></small>
+        </form>
+      </div>
+     
+    </section>
   )
 }
 
